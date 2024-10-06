b8093cdf9a292ef17eb9e11db9edd2c41cf07d01
Ashwin2k3 <ashwin.2k3@gmail.com>
2024-10-06 20:06:11 +0530

    problem 551 b codeforces

--- codeforces/strings/problem_551_B.cpp
+++ codeforces/strings/problem_551_B.cpp
@@ -1,79 +1,95 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <unordered_map>
 
 using namespace std;
 
-
-int countPossible(const string &x, vector<int> &freq) {
-    vector<int> tempFreq = freq;  
-    int count = 0;
-    
-    while (true) {
-        bool canForm = true;
-        for (char ch : x) {
-            if (tempFreq[ch - 'a'] == 0) {
-                canForm = false;
-                break;
-            }
-            tempFreq[ch - 'a']--;
-        }
-        if (canForm)
-            count++;
-        else
-            break;
-    }
-    return count;
-}
-
-
-string constructResult(string &a, string &b, string &c) {
-    
-    vector<int> freq(26, 0);
-    for (char ch : a)
-        freq[ch - 'a']++;
-    
-    
-    int maxB = countPossible(b, freq);
-    int maxC = countPossible(c, freq);
-    
-    
-    vector<int> finalFreq = freq;  
-    string result = "";
-    
-    
-    while (countPossible(b, finalFreq) > 0) {
-        result += b;
-        for (char ch : b)
-            finalFreq[ch - 'a']--;
-    }
-
-    
-    while (countPossible(c, finalFreq) > 0) {
-        result += c;
-        for (char ch : c)
-            finalFreq[ch - 'a']--;
-    }
-
-    
-    for (int i = 0; i < 26; i++) {
-        result += string(finalFreq[i], 'a' + i);
-    }
-
-    return result;
-}
-
 int main() {
-    
     string a, b, c;
     cin >> a >> b >> c;
 
-    
-    string result = constructResult(a, b, c);
+    // Frequency array for the characters in 'a'
+    vector<int> freq(26, 0);
+    for (char ch : a) {
+        freq[ch - 'a']++;
+    }
 
-    
+    // Function to calculate how many times we can form string x from the freq array
+    auto canForm = [&](const string &x) {
+        int cnt = 1e9; // Set to a large number initially
+        vector<int> needed(26, 0); // Frequency of characters needed for 'x'
+        for (char ch : x) {
+            needed[ch - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (needed[i] > 0) {
+                cnt = min(cnt, freq[i] / needed[i]); // How many times we can form 'x'
+            }
+        }
+        return cnt;
+    };
+
+    // Calculate maximum number of `b` and `c` we can form
+    int maxB = canForm(b);
+    int maxC = canForm(c);
+
+    int bestB = 0, bestC = 0;
+    int maxCombined = 0;
+
+    // Try different counts of `b` and `c` to maximize the total substrings
+    for (int i = 0; i <= maxB; i++) {
+        vector<int> tempFreq = freq; // Temporary frequency for this configuration
+        // Decrease for `i` occurrences of `b`
+        for (char ch : b) {
+            tempFreq[ch - 'a'] -= i;
+        }
+
+        // Check how many `c` we can form after using `i` occurrences of `b`
+        int possibleC = 1e9;
+        vector<int> neededC(26, 0);
+        for (char ch : c) {
+            neededC[ch - 'a']++;
+        }
+        for (int j = 0; j < 26; j++) {
+            if (neededC[j] > 0) {
+                possibleC = min(possibleC, tempFreq[j] / neededC[j]);
+            }
+        }
+
+        // Update the best configuration of `b` and `c`
+        if (i + possibleC > maxCombined) {
+            maxCombined = i + possibleC;
+            bestB = i;
+            bestC = possibleC;
+        }
+    }
+
+    // Now, we have the optimal number of `b` and `c` to use (bestB and bestC)
+    // Construct the result string
+    string result = "";
+
+    // Use bestB occurrences of `b`
+    for (int i = 0; i < bestB; i++) {
+        result += b;
+        for (char ch : b) {
+            freq[ch - 'a']--;
+        }
+    }
+
+    // Use bestC occurrences of `c`
+    for (int i = 0; i < bestC; i++) {
+        result += c;
+        for (char ch : c) {
+            freq[ch - 'a']--;
+        }
+    }
+
+    // Append remaining characters in any order
+    for (int i = 0; i < 26; i++) {
+        result += string(freq[i], 'a' + i);
+    }
+
+    // Output the result
     cout << result << endl;
 
     return 0;
